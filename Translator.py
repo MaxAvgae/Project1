2e2a5bd7ece613c5f3b64f522230fe6938204bf9
MaxAvgae <maxavagerus@gmail.com>
2023-06-08 02:15:17 +0300

    !

--- Translator.py
+++ Translator.py
@@ -2,4 +2,4 @@ from transformers import pipeline
 
 translator = pipeline("translation_ru_to_en", "Helsinki-NLP/opus-mt-mul-en")
 
-translator('Девочки, я страдаю')
+translator('Девочки, страдаю от учебы!!!!!')
